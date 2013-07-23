0ccfc0ec8ce82226e5b61541db09cf47497468cd
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-07-24 00:57:12 +0100

    Started playing with asynchronous workflows and FTP

---
+++ AsyncFtp/CopyFiles.fs
@@ -0,0 +1,44 @@
+﻿namespace AsyncFtp
+
+open System.IO
+open System.Net
+open System.Text
+open Microsoft.FSharp.Control.WebExtensions
+open Microsoft.FSharp.Control.StreamReaderExtensions
+
+type public CopyFiles() =
+
+    member public x.UploadFile serverIpAddress directory fileName (fileContents : byte[]) =
+        let uri = sprintf "ftp://%s/%s/%s" serverIpAddress directory fileName
+        let nc = new NetworkCredential("env6ftp", "w1nd0w5.")
+        let request = WebRequest.Create(uri, Method = WebRequestMethods.Ftp.UploadFile, Credentials = nc) :?> FtpWebRequest
+        request.KeepAlive <- false
+        begin
+            use requestStream = request.GetRequestStream()
+            use stream = new MemoryStream(fileContents)
+            stream.CopyTo(requestStream)
+        end
+        use response = request.GetResponse()
+        use responseStream = response.GetResponseStream()
+        use reader = new StreamReader(responseStream)
+        reader.ReadToEnd()
+
+    member public x.UploadFiles n serverIpAddress directory fileNameBase fileContents =
+        let computations = [for i in 1..n -> x.UploadFileAsync serverIpAddress directory (sprintf "%s%d.txt" fileNameBase i) fileContents]
+        Async.Parallel computations |> Async.RunSynchronously
+
+    member private x.UploadFileAsync serverIpAddress directory fileName fileContents =
+        async {
+            let uri = sprintf "ftp://%s/%s/%s" serverIpAddress directory fileName
+            let nc = new NetworkCredential("env6ftp", "w1nd0w5.")
+            let request = WebRequest.Create(uri, Method = WebRequestMethods.Ftp.UploadFile, Credentials = nc) :?> FtpWebRequest
+            request.KeepAlive <- false
+            request.UseBinary <- false
+            use requestStream = request.GetRequestStream()
+            do! requestStream.AsyncWrite(fileContents)
+            requestStream.Close()
+            use! response = request.AsyncGetResponse()
+            use responseStream = response.GetResponseStream()
+            use reader = new StreamReader(responseStream)
+            return! reader.AsyncReadToEnd()
+        }
---
+++ AsyncFtpClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace AsyncFtpClient
+{
+    // ReSharper disable UnusedVariable
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            const string fileContents = "This file was uploaded via F#";
+            var bytes = Encoding.UTF8.GetBytes(fileContents);
+
+            var x = new AsyncFtp.CopyFiles();
+            var result1 = x.UploadFile("192.168.0.133", "LoopMonTest", "FSharpFileSync.txt", bytes);
+            var result2 = x.UploadFiles(15, "192.168.0.133", "LoopMonTest", "FSharpFileAsync", bytes);
+        }
+    }
+}
