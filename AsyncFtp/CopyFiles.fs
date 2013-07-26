7eb6653b1e00ba0b70c2783ee001de9e02a83b25
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-07-26 10:50:17 +0100

    Started work on CopyFiles method using async workflows

--- AsyncFtp/CopyFiles.fs
+++ AsyncFtp/CopyFiles.fs
@@ -4,6 +4,8 @@ open System
 open System.IO
 open System.Net
 open System.Text
+open System.Collections.Concurrent
+open System.Collections.Generic
 open Microsoft.FSharp.Control.WebExtensions
 open Microsoft.FSharp.Control.StreamReaderExtensions
 
@@ -17,7 +19,9 @@ type public CopyFiles() =
 
     let notificationEvent = new Event<NotificationEventDelegate, NotificationEventArgs>()
 
-    member private x.Notify(message) = notificationEvent.Trigger(x, new NotificationEventArgs(message))
+    member private x.Notify(message) =
+        let mtid = System.Threading.Thread.CurrentThread.ManagedThreadId
+        notificationEvent.Trigger(x, new NotificationEventArgs(sprintf "[%d] %s" mtid message))
 
     [<CLIEvent>]
     member public x.Notification = notificationEvent.Publish
@@ -26,7 +30,8 @@ type public CopyFiles() =
         let uri = sprintf "ftp://%s/%s/%s" serverIpAddress directory fileName
         let nc = new NetworkCredential("env6ftp", "w1nd0w5.")
         let request = WebRequest.Create(uri, Method = WebRequestMethods.Ftp.UploadFile, Credentials = nc) :?> FtpWebRequest
-        request.KeepAlive <- false
+        request.KeepAlive <- true
+        request.UseBinary <- true
         begin
             use requestStream = request.GetRequestStream()
             use stream = new MemoryStream(fileContents)
@@ -44,20 +49,57 @@ type public CopyFiles() =
         x.Notify "Ending UploadFiles"
         result
 
+    member public x.CopyFiles (dict:ConcurrentDictionary<string, byte[]>) (listing:IList<string>) srcIp srcDir dstIp dstDir =
+        x.Notify "Beginning CopyFiles"
+        let computations = [
+            for fileName in dict.Keys -> async {
+                if not (listing.Contains(fileName)) then
+                    let fileContents = dict.[fileName]
+                    if fileContents = null then
+                        let! fileContents2 = x.DownloadFile srcIp srcDir fileName
+                        dict.[fileName] <- fileContents2
+                    let! junk = x.UploadFileAsync dstIp dstDir fileName dict.[fileName]
+                    ()
+            }
+        ]
+        Async.Parallel computations |> Async.RunSynchronously |> ignore
+        x.Notify "Ending CopyFiles"
+        ()
+
     member private x.UploadFileAsync serverIpAddress directory fileName fileContents =
         async {
-            x.Notify (sprintf "Beginning (%s)" fileName)
+            x.Notify (sprintf "Beginning UploadFileAsync (%s)" fileName)
             let uri = sprintf "ftp://%s/%s/%s" serverIpAddress directory fileName
             let nc = new NetworkCredential("env6ftp", "w1nd0w5.")
             let request = WebRequest.Create(uri, Method = WebRequestMethods.Ftp.UploadFile, Credentials = nc) :?> FtpWebRequest
-            request.KeepAlive <- false
-            request.UseBinary <- false
-            use requestStream = request.GetRequestStream()
-            do! requestStream.AsyncWrite(fileContents)
-            requestStream.Close()
+            request.KeepAlive <- true
+            request.UseBinary <- true
+            request.ConnectionGroupName <- "UploadFileAsync"
+            do! async {
+                use requestStream = request.GetRequestStream()
+                do! requestStream.AsyncWrite(fileContents)
+            }
             use! response = request.AsyncGetResponse()
             use responseStream = response.GetResponseStream()
             use reader = new StreamReader(responseStream)
-            x.Notify (sprintf "Ending (%s)" fileName)
-            return! reader.AsyncReadToEnd()
+            let! result = reader.AsyncReadToEnd()
+            x.Notify (sprintf "Ending UploadFileAsync (%s)" fileName)
+            return result
+        }
+
+    member private x.DownloadFile srcIp srcDir fileName = 
+        async {
+            x.Notify (sprintf "Beginning DownloadFile (%s)" fileName)
+            let uri = sprintf "ftp://%s/%s/%s" srcIp srcDir fileName
+            let nc = new NetworkCredential("env6ftp", "w1nd0w5.")
+            let request = WebRequest.Create(uri, Method = WebRequestMethods.Ftp.DownloadFile, Credentials = nc) :?> FtpWebRequest
+            request.KeepAlive <- true
+            request.UseBinary <- true
+            request.ConnectionGroupName <- "UploadFileAsync"
+            use! response = request.AsyncGetResponse()
+            use responseStream = response.GetResponseStream()
+            use memoryStream = new MemoryStream()
+            responseStream.CopyTo(memoryStream)
+            x.Notify (sprintf "Ending DownloadFile (%s)" fileName)
+            return memoryStream.ToArray()
         }
--- AsyncFtpClient/Program.cs
+++ AsyncFtpClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
 using System.Text;
 
 namespace AsyncFtpClient
@@ -9,13 +11,33 @@ namespace AsyncFtpClient
     {
         private static void Main()
         {
-            const string fileContents = "This file was uploaded via F#";
-            var bytes = Encoding.UTF8.GetBytes(fileContents);
+            System.Net.ServicePointManager.DefaultConnectionLimit = 10;
+
+            //const string fileContents = "This file was uploaded via F#";
+            //var bytes = Encoding.UTF8.GetBytes(fileContents);
 
             var x = new AsyncFtp.CopyFiles();
-            var result1 = x.UploadFile("192.168.0.133", "LoopMonTest", "FSharpFileSync.txt", bytes);
             x.Notification += (s, e) => Console.WriteLine(e.Message);
-            var result2 = x.UploadFiles(15, "192.168.0.133", "LoopMonTest", "FSharpFileAsync", bytes);
+
+            //var result1 = x.UploadFile("192.168.0.133", "LoopMonTest", "FSharpFileSync.txt", bytes);
+            //var result2 = x.UploadFiles(15, "192.168.0.133", "LoopMonTest", "FSharpFileAsync", bytes);
+
+            var dict = new ConcurrentDictionary<string, byte[]>();
+            dict["file1.txt"] = null;
+            dict["file2.txt"] = null;
+            dict["file3.txt"] = null;
+
+            var listing = new[]
+                {
+                    "file3.txt",
+                    "file4.txt"
+                }.ToList();
+
+            x.CopyFiles(
+                dict,
+                listing,
+                "192.168.0.133", "LoopMonTest/Src",
+                "192.168.0.133", "LoopMonTest/Dst");
         }
     }
 }
